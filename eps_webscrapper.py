b8766a8386b1ba7c0622a6e6f8d38c8786fe6ec4
Ryan Walter Lee <ryanwalterlee@gmail.com>
2024-05-22 01:27:07 +0800

    Change website to companiesmarketcap

--- eps_webscrapper.py
+++ eps_webscrapper.py
@@ -17,27 +17,51 @@ from selenium.webdriver.common.action_chains import ActionChains
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
-def scrape_eps():
+def scrape_eps(ticker):
     
     # Navigate to the specified webpage
-    driver.get("https://stockvalueinsights.com/AAPL/financials/")
+    driver.get("https://companiesmarketcap.com")
 
     # wait until income statement eps row loads
-    row = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "div[comp-id='190']"))
+    search_bar = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "input[id='search-input']"))
     )
 
-    # find all child elements in the parent div and put into an array
-    child_divs = row.find_elements(By.CSS_SELECTOR, "div[role='gridcell']")
+    # enter ticker in search bar
+    search_bar.send_keys(ticker)
+    
+    dropdown_menu = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "div[id='typeahead-search-results']"))
+    )
 
-    # extract actual eps from child elements
-    values = [grid_cell.text for grid_cell in child_divs]
+    # wait for dropdown menu to drop
+    time.sleep(3)
+
+    # select first option in dropdown
+    ticker_to_click = dropdown_menu.find_elements(By.XPATH, ".//*")[0]
+    href = ticker_to_click.get_attribute('href')
+    href = href.replace('/marketcap/', '/eps/')
+    
+    # jump straight to the page to avoid ads
+    driver.get(href)
+    
+    # Locate the table
+    table = driver.find_element(By.CSS_SELECTOR, "table.table")
+
+    # Locate the rows in the table body
+    rows = table.find_elements(By.XPATH, "./tbody/tr")
+
+    # Extract the second <td> from each row, which contains the dollar amounts
+    historical_eps = []
+    for row in rows:
+        td_element = row.find_elements(By.TAG_NAME, "td")[1]  # index 1 for the second <td>
+        historical_eps.append(td_element.text)
 
     driver.quit()
 
-    return values
+    return historical_eps
 
-print(scrape_eps())
+print(scrape_eps('aapl'))
 
 
 
